278a24872e4aa6a79d197f82212238ee4ffa8845
Hassan Bodraa <134235534+Hassonax@users.noreply.github.com>
2026-06-19 16:21:17 +0200

    Add files via upload
    تحويل منswitch الى if

---
+++ 4th/4Th.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, m = 0, c = 0, k = 0;
+
+    for (i = 1; i <= 5; i++)
+    {
+        if (i == 1)
+        {
+            c++;
+        }
+
+        else if(i == 1 || i == 3)
+        {
+            k++;
+        }else{
+
+        m++;};
+    }
+
+    printf("m=%d\nc=%d\nk=%d\n", m, c, k);
+    return 0;
+}
