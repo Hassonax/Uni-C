fff21f798cb044223c0ab8914c853ba1a4948953
Hassan Bodraa <134235534+Hassonax@users.noreply.github.com>
2026-06-19 15:32:48 +0200

    Add files via upload

---
+++ zweite.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char sn[30];
+    int length;
+    int i;
+    char k;
+
+    printf("Please enter student name: ");
+    fgets(sn, sizeof(sn), stdin);
+
+    printf("\n------------\n");
+    puts(sn);
+
+    length = strlen(sn)-1;
+
+    for(i = 0;i <= length; i++)
+    {
+        k = sn[i];
+        printf("\n%c", k);
+    }
+
+    printf("\n length is %d", length);
+    return 0;
+}
