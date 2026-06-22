e6a78525e77e4860d7c93dbd675f606d9ec46979
Hassan Bodraa <134235534+Hassonax@users.noreply.github.com>
2026-06-22 23:33:36 +0200

    Add files via upload

---
+++ 20/20th.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+main(){
+
+ int X[5]={5,-5,0,8,3};
+ int smallest,tempstorage ,i,j;
+
+ for(i=0;i<=4;i++){
+    for(j=0;j<=4;j++){
+        if(i!=j){
+            if(X[i]<X[j]){
+                tempstorage = X[i];
+                X[i]=X[j];
+                X[j]=tempstorage;
+            }
+        }
+    }
+ }
+i=0;
+ for(i=0;i<=4;i++){
+    printf("%i",X[i]);
+ }
+
+
+}
+
