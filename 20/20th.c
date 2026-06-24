58ba0b0af88eaeea25dd6b0b91237c53771429c0
Hassan Bodraa <134235534+Hassonax@users.noreply.github.com>
2026-06-24 20:00:54 +0200

    Refactor sorting logic in 20th.c

--- 20/20th.c
+++ 20/20th.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
+
 main(){
 
- int X[5]={5,-5,0,8,3};
- int smallest,tempstorage ,i,j;
+    int X[5] = {5, -5, 0, 8, 3};
+    int tempstorage, i, j;
 
- for(i=0;i<=4;i++){
-    for(j=0;j<=4;j++){
-        if(i!=j){
-            if(X[i]<X[j]){
+    for(i = 0; i < 4; i++){
+        for(j = i + 1; j <= 4; j++){
+            if(X[i] > X[j]){
                 tempstorage = X[i];
-                X[i]=X[j];
-                X[j]=tempstorage;
+                X[i] = X[j];
+                X[j] = tempstorage;
             }
         }
     }
- }
-i=0;
- for(i=0;i<=4;i++){
-    printf("%i",X[i]);
- }
 
+    for(i = 0; i <= 4; i++){
+        printf("%i ", X[i]);
+    }
 
 }
-
